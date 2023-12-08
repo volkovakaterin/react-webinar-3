3e686684029a9d3545a1c219037875b31e904191
Ekaterina Volkova <volkovakaterin@gmail.com>
2023-12-08 22:14:30 +0300

    new commit

--- src/app/main/index.js
+++ src/app/main/index.js
@@ -1,4 +1,4 @@
-import {memo, useCallback, useEffect} from 'react';
+import { memo, useCallback, useEffect, useState } from "react";
 import Item from "../../components/item";
 import PageLayout from "../../components/page-layout";
 import Head from "../../components/head";
@@ -6,42 +6,74 @@ import BasketTool from "../../components/basket-tool";
 import List from "../../components/list";
 import useStore from "../../store/use-store";
 import useSelector from "../../store/use-selector";
+import Pagination from "../../components/pagination";
+import { Route, Routes } from "react-router-dom";
 
 function Main() {
-
   const store = useStore();
+  const [page, setPage] = useState(1);
+  const perPage = 10;
 
   useEffect(() => {
     store.actions.catalog.load();
   }, []);
 
-  const select = useSelector(state => ({
+  useEffect(() => {
+    store.actions.catalog.loadPag(page, perPage);
+  }, [page]);
+
+  const select = useSelector((state) => ({
     list: state.catalog.list,
+    listPag: state.catalog.listPag,
+    count: state.catalog.count,
     amount: state.basket.amount,
-    sum: state.basket.sum
+    sum: state.basket.sum,
   }));
 
   const callbacks = {
     // Добавление в корзину
-    addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store]),
+    addToBasket: useCallback(
+      (_id) => store.actions.basket.addToBasket(_id),
+      [store]
+    ),
     // Открытие модалки корзины
-    openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store]),
-  }
+    openModalBasket: useCallback(
+      () => store.actions.modals.open("basket"),
+      [store]
+    ),
+  };
 
   const renders = {
-    item: useCallback((item) => {
-      return <Item item={item} onAdd={callbacks.addToBasket}/>
-    }, [callbacks.addToBasket]),
+    item: useCallback(
+      (item) => {
+        return <Item item={item} onAdd={callbacks.addToBasket} />;
+      },
+      [callbacks.addToBasket]
+    ),
   };
 
   return (
     <PageLayout>
-      <Head title='Магазин'/>
-      <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount}
-                  sum={select.sum}/>
-      <List list={select.list} renderItem={renders.item}/>
+      <Head title="Магазин" />
+      <BasketTool
+        onOpen={callbacks.openModalBasket}
+        amount={select.amount}
+        sum={select.sum}
+      />
+      <Routes>
+        <Route
+          path="/"
+          element={<List list={select.listPag} renderItem={renders.item} />}
+        />
+        <Route path={`/product-page/${id}`} element={<ProductPage />} />
+      </Routes>
+      <Pagination
+        itemsQnty={select.count}
+        perPage={perPage}
+        setPage={setPage}
+        currentPage={page}
+      />
     </PageLayout>
-
   );
 }
 
--- src/store/catalog/index.js
+++ src/store/catalog/index.js
@@ -1,26 +1,48 @@
-import {codeGenerator} from "../../utils";
+import { codeGenerator } from "../../utils";
 import StoreModule from "../module";
 
 class Catalog extends StoreModule {
-
   constructor(store, name) {
     super(store, name);
-    this.generateCode = codeGenerator(0)
+    this.generateCode = codeGenerator(0);
   }
 
   initState() {
     return {
-      list: []
-    }
+      list: [],
+      listPag: [],
+      count: null,
+    };
   }
 
   async load() {
-    const response = await fetch('/api/v1/articles');
+    const response = await fetch("/api/v1/articles?limit=*");
     const json = await response.json();
-    this.setState({
-      ...this.getState(),
-      list: json.result.items
-    }, 'Загружены товары из АПИ');
+    console.log(json);
+    this.setState(
+      {
+        ...this.getState(),
+        list: json.result.items,
+      },
+      "Загружены ВСЕ товары из АПИ"
+    );
+  }
+
+  async loadPag(page, perPage) {
+    const numberItem = (page - 1) * 10;
+    const response = await fetch(
+      `/api/v1/articles?limit=${perPage}&skip=${numberItem}&fields=items(_id, title, price),count`
+    );
+    const json = await response.json();
+    console.log(json);
+    this.setState(
+      {
+        ...this.getState(),
+        listPag: json.result.items,
+        count: json.result.count,
+      },
+      "Пагинация"
+    );
   }
 }
 
